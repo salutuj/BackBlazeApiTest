eed7b7bdf96de409c7c0b2b867deff0f177bbbf8
Pawel Niewiadomski <pniewiadomski@si-ecommerce.pl>
2017-06-06 00:24:04 +0200

    hello backblaze

--- src/main/java/eu/pawelniewiadomski/java/backblaze/apitest/BackBlazeHelloWorld.java
+++ src/main/java/eu/pawelniewiadomski/java/backblaze/apitest/BackBlazeHelloWorld.java
@@ -1,8 +1,13 @@
 package eu.pawelniewiadomski.java.backblaze.apitest;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import synapticloop.b2.B2ApiClient;
+import synapticloop.b2.BucketType;
 import synapticloop.b2.exception.B2ApiException;
+import synapticloop.b2.response.B2BucketResponse;
 
 
 public class BackBlazeHelloWorld {
@@ -15,14 +20,16 @@ public class BackBlazeHelloWorld {
 			B2ApiClient b2ApiClient = new B2ApiClient();
 			b2ApiClient.authenticate(b2AccountId, b2ApplicationKey);
 
+			b2ApiClient.listBuckets().forEach(System.out::println);
+			
 			// now create a private bucket
-			B2BucketResponse createPrivateBucket = b2ApiClient.createBucket("super-secret-bucket" , BucketType.allPrivate);
+			//B2BucketResponse createPrivateBucket = b2ApiClient.createBucket("super-secret-bucket" , BucketType.allPrivate);
 
 			// or a public one
-			B2BucketResponse createPublicBucket = b2ApiClient.createBucket("everyone-has-access" , BucketType.allPublic);
+			//B2BucketResponse createPublicBucket = b2ApiClient.createBucket("everyone-has-access" , BucketType.allPublic);
 
 			// upload a file to the private bucket
-			b2ApiClient.uploadFile(createPrivateBucket.getBucketId(), "myfile.txt", new File("/tmp/temporary-file.txt"));
+			//apiclient uploadFile(createPrivateBucket.getBucketId(), "myfile.txt", new File("/tmp/temporary-file.txt"));
 
 		} catch(B2ApiException | IOException ex) {
 			ex.printStackTrace();
